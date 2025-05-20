0aa5ca2ea207d2c93f87033026e5ee654155031e
Owen Craston <owen.craston@consensys.net>
2025-05-20 15:48:47 +0100

    disable supermaven

--- lua/plugins/supermaven.lua
+++ lua/plugins/supermaven.lua
@@ -1,20 +1,24 @@
-return {
-  -- Extend nvim-cmp with supermaven source
-  {
-    "hrsh7th/nvim-cmp",
-    opts = function(_, opts)
-      local cmp = require("cmp")
-      -- Add supermaven as a source for nvim-cmp
-      opts.sources = cmp.config.sources(vim.list_extend(opts.sources, {
-        { name = "supermaven" },
-      }))
-    end,
-  },
-  -- Setup supermaven plugin
-  {
-    "supermaven-inc/supermaven-nvim",
-    config = function()
-      require("supermaven-nvim").setup({})
-    end,
-  },
-}
+-- Commented out Supermaven configuration
+-- return {
+--   -- Extend nvim-cmp with supermaven source
+--   {
+--     "hrsh7th/nvim-cmp",
+--     opts = function(_, opts)
+--       local cmp = require("cmp")
+--       -- Add supermaven as a source for nvim-cmp
+--       opts.sources = cmp.config.sources(vim.list_extend(opts.sources, {
+--         { name = "supermaven" },
+--       }))
+--     end,
+--   },
+--   -- Setup supermaven plugin
+--   {
+--     "supermaven-inc/supermaven-nvim",
+--     config = function()
+--       require("supermaven-nvim").setup({})
+--     end,
+--   },
+-- }
+
+-- Return empty table to keep the file valid
+return {}
